74bccbef344a8180ed183525c4e5b0a419bf488d
Komal6627 <komalkilledar1996@gmail.com>
2022-09-19 16:14:09 +0530

    feat: post api added

--- src/mapdata/mapdata.controller.ts
+++ src/mapdata/mapdata.controller.ts
@@ -6,18 +6,74 @@ import {
   Patch,
   Param,
   Delete,
+  UseInterceptors,
+  UploadedFile,
+  Res,
 } from '@nestjs/common';
 import { MapdataService } from './mapdata.service';
 import { CreateMapdatumDto } from './dto/create-mapdatum.dto';
 import { UpdateMapdatumDto } from './dto/update-mapdatum.dto';
+import { FileInterceptor } from '@nestjs/platform-express/multer/interceptors';
+import { diskStorage } from 'multer';
+import { extname } from 'path';
+import { readFileSync } from 'fs';
+import { parse } from 'papaparse';
 
 @Controller('mapdata')
 export class MapdataController {
+  filepath: any;
   constructor(private readonly mapdataService: MapdataService) {}
 
   @Post()
   create(@Body() createMapdatumDto: CreateMapdatumDto) {
     return this.mapdataService.create(createMapdatumDto);
+  }
+
+  // @Post('data')
+  // @UseInterceptors(
+  //   FileInterceptor('file', {
+  //     storage: diskStorage({
+  //       destination: './src/filedata',
+  //       filename: (req, file, callback) => {
+  //         const uniqueSuffix =
+  //           Date.now() + '-' + Math.round(Math.random() * 1e9);
+  //         const ext = extname(file.originalname);
+  //         // const filename = `${image.originalname}-${uniqueSuffix}${ext}`;
+  //         const filename = `${uniqueSuffix}${ext}`;
+  //         callback(null, filename);
+  //       },
+  //     }),
+  //   }),
+  // )
+  // handleupload(@UploadedFile() file: Express.Multer.File) {
+  //   this.filepath = file.path;
+  //   console.log('file', file);
+  //   console.log('path', file.path);
+  //   return 'file upload API';
+  // }
+  // @Get('/file/:filepath')
+  // seeUploadedFile(@Param('filepath') file, @Res() res) {
+  //   return res.sendFile(file, { root: './src/filedata' });
+  // }
+
+  @Post('data')
+  @UseInterceptors(
+    FileInterceptor('file_asset', {
+      storage: diskStorage({
+        destination: 'filedata',
+      }),
+    }),
+  )
+  async uploadFile() {
+    const csvFile = readFileSync('filedata/1.csv');
+    const csvData = csvFile.toString();
+    const parsedCsv = await parse(csvData, {
+      header: true,
+      skipEmptyLines: true,
+      transformHeader: (header) => header.toLowerCase().replace('#', '').trim(),
+      complete: (results) => results.data,
+    });
+    console.log(parsedCsv);
   }
 
   @Get()
